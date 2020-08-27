81c6efa03797a068ce2801e6cfb64837aa168af6
JoshiRaez <joseraezrodriguez@gmail.com>
2020-08-27 02:08:39 +0200

    Error, option, result, and move semantics completed

--- exercises/error_handling/result1.rs
+++ exercises/error_handling/result1.rs
@@ -1,8 +1,6 @@
 // result1.rs
 // Make this test pass! Execute `rustlings hint result1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -14,7 +12,12 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        match (value < 0, value == 0) {
+            (true, _)   =>  Err(CreationError::Negative),
+            (_ , true)  =>  Err(CreationError::Zero),
+            _           =>  Ok(PositiveNonzeroInteger(value as u64)),
+        
+        }
     }
 }
 
